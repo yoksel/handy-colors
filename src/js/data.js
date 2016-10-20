5973a4ea40611741030079d9581792596c2729c9
yokselzok <yokselzok@gmail.com>
2016-10-21 01:45:45 +0300

    Add more palettes

--- src/js/data.js
+++ src/js/data.js
@@ -1,69 +1,9 @@
 var palettes = [];
 
 palettes[ palettes.length ] = {
-  title: 'Sky',
-  author: {
-    twitter: 'yoksel',
-    github: ''
-  },
-  colors: [
-    'lightcyan',
-    'paleturquoise',
-    'skyblue',
-    'cornflowerblue',
-    'royalblue'
-  ],
-};
-
-palettes[ palettes.length ] = {
-  title: 'Sand',
-  author: {
-    twitter: 'yoksel',
-    github: ''
-  },
-  colors: [
-    'lightgoldenrodyellow',
-    'palegoldenrod',
-    'goldenrod',
-    'darkgoldenrod',
-    'saddlebrown'
-  ],
-};
-
-palettes[ palettes.length ] = {
-  title: 'Sea I',
-  author: {
-    twitter: 'yoksel',
-    github: ''
-  },
-  colors: [
-    'beige',
-    'palegoldenrod',
-    'cadetblue',
-    'teal',
-    'darkslategray'
-  ],
-};
-
-palettes[ palettes.length ] = {
-  title: 'Sea II',
-  author: {
-    twitter: 'yoksel',
-    github: ''
-  },
-  colors: [
-    'lemonchiffon',
-    'mediumturquoise',
-    'lightseagreen',
-    'teal',
-    'darkslategray',
-  ],
-};
-
-palettes[ palettes.length ] = {
   title: 'Sunrise',
   author: {
-    twitter: 'yoksel',
+    twitter: 'yoksel_en',
     github: ''
   },
   colors: [
@@ -78,7 +18,7 @@ palettes[ palettes.length ] = {
 palettes[ palettes.length ] = {
   title: 'Bright',
   author: {
-    twitter: 'yoksel',
+    twitter: 'yoksel_en',
     github: ''
   },
   colors: [
@@ -93,8 +33,7 @@ palettes[ palettes.length ] = {
 palettes[ palettes.length ] = {
   title: 'Autumn',
   author: {
-    twitter: 'yoksel',
-    github: 'yoksel'
+    twitter: 'yoksel_en'
   },
   colors: [
     'sandybrown',
@@ -106,10 +45,141 @@ palettes[ palettes.length ] = {
 };
 
 palettes[ palettes.length ] = {
+  title: 'Felt pens',
+  author: {
+    twitter: 'yoksel_en',
+    github: 'yoksel_en'
+  },
+  colors: [
+    'purple',
+    'crimson',
+    'gold',
+    'teal',
+    'darkslategray',
+  ],
+};
+
+palettes[ palettes.length ] = {
+  title: 'Sea I',
+  author: {
+    twitter: 'yoksel_en',
+    github: ''
+  },
+  colors: [
+    'beige',
+    'palegoldenrod',
+    'cadetblue',
+    'teal',
+    'darkslategray'
+  ],
+};
+
+palettes[ palettes.length ] = {
+  title: 'Sea II',
+  author: {
+    twitter: 'yoksel_en',
+    github: ''
+  },
+  colors: [
+    'lemonchiffon',
+    'mediumturquoise',
+    'lightseagreen',
+    'teal',
+    'darkslategray',
+  ],
+};
+
+palettes[ palettes.length ] = {
+  title: 'Red',
+  author: {
+    twitter: 'yoksel_en',
+    github: ''
+  },
+  colors: [
+    'darkred',
+    'brown',
+    'firebrick',
+    'crimson',
+    'red',
+  ],
+};
+
+palettes[ palettes.length ] = {
+  title: 'Pink',
+  author: {
+    twitter: 'yoksel_en'
+  },
+  colors: [
+    'mistyrose',
+    'pink',
+    'palevioletred',
+    'deeppink',
+    'mediumvioletred',
+  ],
+};
+
+palettes[ palettes.length ] = {
+  title: 'Yellow',
+  author: {
+    twitter: 'yoksel_en'
+  },
+  colors: [
+    'goldenrod',
+    'gold',
+    'khaki',
+    'lemonchiffon',
+    'lightyellow',
+  ],
+};
+
+palettes[ palettes.length ] = {
+  title: 'Sand',
+  author: {
+    twitter: 'yoksel_en',
+    github: ''
+  },
+  colors: [
+    'lightgoldenrodyellow',
+    'palegoldenrod',
+    'goldenrod',
+    'darkgoldenrod',
+    'saddlebrown'
+  ],
+};
+
+palettes[ palettes.length ] = {
+  title: 'Sky',
+  author: {
+    twitter: 'yoksel_en',
+    github: ''
+  },
+  colors: [
+    'lightcyan',
+    'paleturquoise',
+    'skyblue',
+    'cornflowerblue',
+    'royalblue'
+  ],
+};
+
+palettes[ palettes.length ] = {
+  title: 'Blue',
+  author: {
+    twitter: 'yoksel_en'
+  },
+  colors: [
+    'royalblue',
+    'blue',
+    'mediumblue',
+    'darkblue',
+    'navy',
+  ],
+};
+
+palettes[ palettes.length ] = {
   title: 'Lilac',
   author: {
-    twitter: 'yoksel',
-    github: 'yoksel'
+    github: 'yoksel_en'
   },
   colors: [
     'orchid',
